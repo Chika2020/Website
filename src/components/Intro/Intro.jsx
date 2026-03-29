4e35c25a30a8402eb0eb319124ad28e91e43ce22
Chika Nwachukwu <chika.simon.nwachukwu@gmail.com>
2026-03-29 01:35:03 -0500

    fix es lint issue

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import './responsive-player.scss';
 
-const YoutubeEmbed = ({ embedId }) => {
-  return (
-    <section id="intro" className="section-video-responsive">
-      <div className="video-responsive">
-        <iframe
-          width="853"
-          height="480"
-          src={`https://www.youtube.com/embed/${embedId}`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          title="Embedded youtube"
-        />
-      </div>
-    </section>
-  );
-};
+const YoutubeEmbed = ({ embedId }) => (
+  <section id="intro" className="section-video-responsive">
+    <div className="video-responsive">
+      <iframe
+        width="853"
+        height="480"
+        src={`https://www.youtube.com/embed/${embedId}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  </section>
+);
 export default YoutubeEmbed;
